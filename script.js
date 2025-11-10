5f6c1eea141f84d4db92e792e4ea1789366d2bf9
jose-labo <labobedisedilame@gmail.com>
2025-11-10 13:42:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function updateTime () {
+function updateClock() {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2,'0');
